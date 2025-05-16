5617ee95d2b21bd2cebf4ab27035e7de400281d7
Wes Clarkson <wesclarksonpt@gmail.com>
2025-05-16 19:40:30 -0400

    configure for github pages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import ServicesCarousel from './components/ServicesCarousel';
@@ -6,12 +5,11 @@ import Testimonials from './components/Testimonials';
 //import FAQAccordion from './components/FAQAccordion';
 import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
-import AdminDashboard from './components/AdminDashboard';
 
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-function HomePage() {
+function App() {
   return (
     <>
       <Navbar />
@@ -21,17 +19,6 @@ function HomePage() {
       <ContactForm />
       <Footer />
     </>
-  );
-}
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-      </Routes>
-    </Router>
   );
 }
 
